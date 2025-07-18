c5edb6d6980a313144cfb0261058669300290a46
nrusinhgh <nrusinh.salesforce@gmail.com>
2025-07-18 19:12:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ session = cnx.session()
 name_on_order = st.text_input("Name on Smoothie : ")
 st.write("Name on your Smoothie will be: ", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect('choose upto 5 ingrediants:', my_dataframe, max_selections = 5)
 
