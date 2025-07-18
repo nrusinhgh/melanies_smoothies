7f7bc5019f7d044f9852728ca6f5f013a1ac997e
nrusinhgh <nrusinh.salesforce@gmail.com>
2025-07-18 17:04:58 +0530

    Update streamlit_app.py
    adding dataframe to put JSON result

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,6 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
